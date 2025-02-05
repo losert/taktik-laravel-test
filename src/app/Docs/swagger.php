f4549f1b1b1973c6f123f1a652cf736aad74eee7
Jiří Losert <losert.jiri@gmail.com>
2025-02-05 11:04:40 +0100

    swagger

---
+++ src/app/Docs/swagger.php
@@ -0,0 +1,16 @@
+<?php
+/**
+ * @OA\Info(
+ *     version="1.0.0",
+ *     title="My API Documentation",
+ *     description="This is the API documentation for My API.",
+ *     @OA\Contact(
+ *         email="your-email@example.com"
+ *     )
+ * )
+ *
+ * @OA\Server(
+ *     url="http://localhost:8000",
+ *     description="Local Development Server"
+ * )
+ */
